17021461fde2189eea7eb9dc30bb1ff5e905fa42
Reche Maynard <reche.maynard95@gmail.com>
2026-01-27 16:43:32 +0000

    Initial commit

---
+++ project.h
@@ -0,0 +1,23 @@
+#ifndef PROJECT_H
+#define PROJECT_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Project;
+}
+QT_END_NAMESPACE
+
+class Project : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Project(QWidget *parent = nullptr);
+    ~Project();
+
+private:
+    Ui::Project *ui;
+};
+#endif // PROJECT_H
